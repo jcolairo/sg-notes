a967d5e1886db249c81b08cfe0b24da40a8b4815
James Colairo <jcolairo@spartaglobal.co>
2017-03-14 12:02:22 +0000

    express server with router and controller

---
+++ week05-day02-node/express-web-server-with-controller/config/router.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+var usersController = require('../controllers/users-controller');
+
+// Action: index
+router.route('/users/')
+  .get(usersController.index)
+// Action: create
+  .post(usersController.create);
+// Action: new
+router.get('/users/new', usersController.new);
+// Action: edit
+router.get('/users/:id/edit', usersController.edit);
+// Action: update
+router.route('/users/:id')
+  .put(usersController.update)
+// Action: show
+  .get(usersController.show)
+// Action: destroy
+  .delete(usersController.destroy);
+
+module.exports = router;
