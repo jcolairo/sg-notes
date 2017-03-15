23689bec7f09b290b636a9077717572afcc302a7
James Colairo <jcolairo@spartaglobal.co>
2017-03-15 20:05:44 +0000

    added mongo to update all the information and made it work in the browser

--- week05-day03-node/express-web-server-mvc/index.js
+++ week05-day03-node/express-web-server-mvc/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = require('./config/router');
 var bodyParser = require('body-parser');
 var layouts = require('express-ejs-layouts');
+var methodOverride = require('method-override');
+var mongoose = require('mongoose');
 var app = express();
 var port = 3000;
+
+mongoose.connect('mongodb://localhost/sg-mvc');
 
 app.set('view engine', 'ejs');
 app.use(function (req, res, next) {
@@ -14,6 +18,15 @@ app.use(function (req, res, next) {
 app.use(layouts);
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(methodOverride(function (req) {
+  console.log('methodOverride check req.body:', req.body );
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    // look in urlencoded POST bodies and delete it
+    var method = req.body._method;
+    delete req.body._method;
+    return method;
+  }
+}));
 
 app.use(router);
 
