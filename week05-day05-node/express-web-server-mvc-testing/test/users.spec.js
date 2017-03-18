30060d7783c8a53831afb009794c101694d64a03
James Colairo <jcolairo@spartaglobal.co>
2017-03-18 13:40:29 +0000

    pulled updated version for testing

--- week05-day05-node/express-web-server-mvc-testing/test/users.spec.js
+++ week05-day05-node/express-web-server-mvc-testing/test/users.spec.js
@@ -9,17 +9,28 @@ var request;
 chai.should();
 chai.use(chaiHttp);
 
+// We are looking for HTML that looks like this:
+// <a href="/users/58cbb8e616f8b0228f71b315">
+// We can the extract the user ID from the `href` attribute using a regex.
+function getFirstUserIdFromUserListHTML(html) {
+  var regEx = /\/users\/[0-9a-f]+/;
+  var result = regEx.exec(html)[0];
+  var pathElements = result.split('/');
+
+  return pathElements[2];
+}
+
 describe('Users', function () {
   beforeEach(function () {
     request = chai.request(app);
   });
 
-  describe('/GET', function () {
+  describe('GET', function () {
     it('should return error for invalid URL GET', function (done) {
       request
         .get('/invalid_url')
         .end(function (err) {
-          expect(err).not.to.be.undefined;
+          expect(err).not.to.be.null;
           done();
         });
     });
@@ -34,15 +45,29 @@ describe('Users', function () {
           done();
         });
     });
-  })
+  });
 
-  describe('/DELETE', function () {
+  describe('DELETE', function () {
     it('should return error for non-existent user id', function (done) {
       request
         .delete('/users/non-existent-user-id')
         .end(function (err, res) {
           res.should.have.status(404);
           done();
+        });
+    });
+    it('should return correct result for existing user', function (done) {
+      request
+        .get('/users')
+        .end(function (err, res) {
+          var userId = getFirstUserIdFromUserListHTML(res.text);
+
+          request
+            .delete('/users/' + userId)
+            .end(function (err, res) {
+              res.should.have.status(200);
+              done();
+            });
         });
     });
   });
