dfbe65137820c8163aefd9022eafadcdaf71662c
James Colairo <jcolairo@spartaglobal.co>
2017-03-30 20:56:26 +0100

    added new todo, delete button and checkbox delete :)

--- week07-day04-angular-routing-homewok/js/controllers/home.controller.js
+++ week07-day04-angular-routing-homewok/js/controllers/home.controller.js
@@ -1,18 +1,43 @@
 function HomeController() {
   var controller = this;
 
-  controller.title = 'This is the home page';
+  controller.title = 'Show current TODOS';
+
+  controller.addTitle = 'Add new TODOS';
+  controller.addNewTodos = function() {
+    controller.todos.push(controller.newTodos);
+    controller.newTodos = '';
+  };
+
+  controller.updateTodos = function(index) {
+    if(controller.updatedTodosField[index]) {
+      controller.todos[index] = controller.updatedTodosField[index];
+    }
+  };
+
+  controller.deleteTodos = function($index) {
+    controller.todos.splice($index, 1);
+  };
 
   function init() {
-    controller.todos = {
-      '1': 'Something',
-      '2': 'Something Something',
-      '3': 'Something Something Something',
-      '4': 'Something Something Something Something'
-    };
+    controller.todos = ['Something', 'Something Something', 'Something Something Something','Something Something Something Something'];
   }
   init();
 }
+//
+// var myApp = angular.module('myApp', []);
+//
+// myApp.factory('Fact', function() {
+//   return {Field: ''};
+// });
+//
+// myApp.controller('FirstCtrl', function (controller, Fact ) {
+//   controller.Alpha = Fact;
+// });
+//
+// myApp.controller('SecondCtrl', function (controller, Fact) {
+//   controller.Beta = Fact;
+// });
 
 angular
   .module('angularstates')
--- week07-day04-angular-routing-homewok/js/controllers/new.controller.js
+++ week07-day04-angular-routing-homewok/js/controllers/new.controller.js
@@ -3,6 +3,13 @@ function NewController() {
 
   controller.title = 'Add new TODOS';
 
+  // controller.addNewTodos = function() {
+  //   controller.todos.push(controller.newTodos);
+  //   controller.newTodos = '';
+  // };
+
+  controller.todos = ['Something', 'Something Something', 'Something Something Something','Something Something Something Something'];
+
 
   function init() {
 
