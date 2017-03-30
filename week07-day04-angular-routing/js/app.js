0c642576302e2a71dde7618cc900129c3c63e415
James Colairo <jcolairo@spartaglobal.co>
2017-03-30 12:55:15 +0100

    add home state

--- week07-day04-angular-routing/js/app.js
+++ week07-day04-angular-routing/js/app.js
@@ -1,2 +1,13 @@
+function mainRouter($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('home', {
+      url: '/',
+      templateUrl: '../states/home.html'
+    });
+
+  $urlRouterProvider.otherwise('/');
+}
+
 angular
-  .module('angularstates', []);
+  .module('angularstates', ['ui.router'])
+  .config(mainRouter);
