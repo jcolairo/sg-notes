bd431dd019a88f824644720f1dde7be9babe594e
James Colairo <jcolairo@spartaglobal.co>
2017-03-31 16:02:24 +0100

    implement getOne() for ducks

--- week07-day05-angular-crud/angular-crud/frontend/js/controllers/duck.controller.js
+++ week07-day05-angular-crud/angular-crud/frontend/js/controllers/duck.controller.js
@@ -1,20 +1,36 @@
-function DuckController(DuckFactory) {
+function DuckController($stateParams, DuckFactory ) {
   var controller = this;
+
+  controller.getDuck = function() {
+    var duckId = $stateParams.duckId;
+
+    DuckFactory.getOne(duckId).then(
+      function success(response) {
+        console.log('Duck', response);
+        controller.selectedDuck = response.data;
+      },
+      function error(error) {
+        console.log('Error getting duck:', error);
+      }
+    );
+  };
 
   function init() {
     console.log(controller);
+    controller.selectedDuck = undefined;
     controller.allDucks = [];
     //make an API call to retrieve the data
     DuckFactory.getAll().then(
-      function (response) {
+      function success(response) {
         controller.allDucks = response.data;
         console.log('allDucks:', controller.allDucks);
       },
-      function (error) {
+      function error(error) {
         console.log('Error getting ducks', error);
       }
     );
   }
+
   init();
 }
 
