b249dac513caf944d9634401da8d195cb4a1334e
James Colairo <jcolairo@spartaglobal.co>
2017-04-19 16:47:08 +0100

    added remaining tests

--- week10-day03-angular-testing/unit-testing-angularjs-controller/spec/todos.controller.spec.js
+++ week10-day03-angular-testing/unit-testing-angularjs-controller/spec/todos.controller.spec.js
@@ -32,12 +32,39 @@ describe('TodosController', () => {
       controllerToTest.add();
       expect(MockTodosFactory.add).toHaveBeenCalledWith(inputText);
     });
-    it('Should clearn inputText', () => {
+    it('Should clear inputText', () => {
       const inputText = 'new todo 2';
 
       controllerToTest.inputText = inputText;
       controllerToTest.add();
       expect(controllerToTest.inputText).toEqual('');
+    });
+  });
+
+  describe('isSubmitButtonDisabled()', () => {
+    it('should return true when inputText field is empty', () => {
+      expect(controllerToTest.inputText).toEqual('');
+    });
+    it('should return false when inputText field is not empty', () => {
+      controllerToTest.inputText = 'something';
+      expect(!controllerToTest.inputText).toEqual(false);
+    });
+  });
+
+  describe('isClearButtonDisabled()', () => {
+    it('Should return true when list is empty', () => {
+      expect(controllerToTest.list).toEqual([]);
+    });
+    it('Should return false when list is not empty', () => {
+      controllerToTest.inputText = 'something else';
+      expect(!controllerToTest.inputText).toEqual(false);
+    });
+  });
+
+  describe('clear()', () => {
+    it('Should call TodosFactory.clear()', () => {
+      controllerToTest.clear();
+      expect(MockTodosFactory.clear).toHaveBeenCalledWith();
     });
   });
 });
