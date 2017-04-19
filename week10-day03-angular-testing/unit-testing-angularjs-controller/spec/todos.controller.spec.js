5ab2c65939983b9a331401142f124e95766c9dd6
James Colairo <jcolairo@spartaglobal.co>
2017-04-19 15:39:59 +0100

    add test for add() method on todos controller

--- week10-day03-angular-testing/unit-testing-angularjs-controller/spec/todos.controller.spec.js
+++ week10-day03-angular-testing/unit-testing-angularjs-controller/spec/todos.controller.spec.js
@@ -1,19 +1,42 @@
 describe('TodosController', () => {
   let controllerToTest;
+  let MockTodosFactory;
 
   beforeEach(() => {
     module('todosApp');
+    MockTodosFactory = {
+      list: [],
+      add: jasmine.createSpy(),
+      clear: jasmine.createSpy()
+    };
     inject(($controller) => {
       // creating an instance using the controllerToTest
-      controllerToTest = $controller('TodosController');
+      controllerToTest = $controller('TodosController', { TodosFactory: MockTodosFactory });
     });
   });
 
   describe('initialisation', () => {
-    it('Should initialise lsit correctly', () => {
+    it('Should initialise list correctly', () => {
       expect(controllerToTest.list).toEqual([]);
     });
     it('Should initialise inputText correctly', () => {
+      expect(controllerToTest.inputText).toEqual('');
+    });
+  });
+
+  describe('add()', () => {
+    it('Should call TodosFactory.add() with correct parameter', () => {
+      const inputText = 'new todo 1';
+
+      controllerToTest.inputText = inputText;
+      controllerToTest.add();
+      expect(MockTodosFactory.add).toHaveBeenCalledWith(inputText);
+    });
+    it('Should clearn inputText', () => {
+      const inputText = 'new todo 2';
+
+      controllerToTest.inputText = inputText;
+      controllerToTest.add();
       expect(controllerToTest.inputText).toEqual('');
     });
   });
