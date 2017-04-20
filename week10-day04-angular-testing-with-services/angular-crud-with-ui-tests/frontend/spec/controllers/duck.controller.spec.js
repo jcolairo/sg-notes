8e3c001fc0d22590ef8b8d504818d6bdc5d578ad
James Colairo <jcolairo@spartaglobal.co>
2017-04-20 15:15:08 +0100

    set up beforeEach in karma test

--- week10-day04-angular-testing-with-services/angular-crud-with-ui-tests/frontend/spec/controllers/duck.controller.spec.js
+++ week10-day04-angular-testing-with-services/angular-crud-with-ui-tests/frontend/spec/controllers/duck.controller.spec.js
@@ -1,17 +1,36 @@
 describe('DuckController', () => {
   let controllerToTest;
+  let httpBackend;
+  let mock$State;
+  let mock$StateParams;
+  let testDuckId;
 
   beforeEach(() => {
     module('DuckApp');
-    inject(($controller) => {
+    inject(($controller, $httpBackend) => {
       // creating an instance using the controllerToTest
-      controllerToTest = $controller('DuckController');
+      httpBackend = $httpBackend;
+      $httpBackend
+        .when('GET', 'http://localhost:3000/ducks')
+        .respond([{ message: '200 OK' }]);
+      mock$State = {
+        go: jasmine.createSpy()
+      };
+      mock$StateParams = {
+        duckId: testDuckId
+      };
+      controllerToTest = $controller('DuckController', {
+        $stateParams: mock$StateParams,
+        $state: mock$State
+      });
     });
   });
 
   describe('initialisation', () => {
     it('Should do a basic test', () => {
       console.log('got inside the basic test');
+      httpBackend.flush();
+      console.log('in test: allDucks:', controllerToTest.allDucks);
     });
   });
 });
