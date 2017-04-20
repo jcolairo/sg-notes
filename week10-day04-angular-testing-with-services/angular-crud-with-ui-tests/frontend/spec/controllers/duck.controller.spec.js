5af8063b9da548b0c7b941aab73e1a159bb324b7
James Colairo <jcolairo@spartaglobal.co>
2017-04-20 12:44:03 +0100

    added spec files and ran one test that failed

---
+++ week10-day04-angular-testing-with-services/angular-crud-with-ui-tests/frontend/spec/controllers/duck.controller.spec.js
@@ -0,0 +1,17 @@
+describe('DuckController', () => {
+  let controllerToTest;
+
+  beforeEach(() => {
+    module('DuckApp');
+    inject(($controller) => {
+      // creating an instance using the controllerToTest
+      controllerToTest = $controller('DuckController');
+    });
+  });
+
+  describe('initialisation', () => {
+    it('Should do a basic test', () => {
+      console.log('got inside the basic test');
+    });
+  });
+});
